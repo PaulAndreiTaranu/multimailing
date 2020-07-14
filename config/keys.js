4c34103ce2524baa7e7a23a0dbf1330d7a744de2
Paul Andrei <paulandreitaranu@gmail.com>
2020-07-14 20:55:56 +0100

    client

--- config/keys.js
+++ config/keys.js
@@ -3,7 +3,3 @@ if (process.env.NODE_ENV === "production") {
 } else {
    module.exports = require("./dev");
 }
-
-// PROD STUFF
-// googleClient: 762237509201-1kkigf9l8ul4nveojss27ls8q94eth3g.apps.googleusercontent.com
-// googleSecret: -b19oWDHRzllRwoWeAv1WuvP
